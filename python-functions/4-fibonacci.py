9a3e844294a370b1d600d6ebc57908fef2662251
Emmanuel047 <emwendwa007@gmail.com>
2023-07-20 08:57:46 +0300

    6-pass

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -9,3 +9,15 @@ def fibonacci_sequence(n):
         fib_sequence.append(next_number)
 
     return fib_sequence
+#!/usr/bin/env python3
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    else:
+        fib_iseq = [0, 1]
+        while len(fib_iseq) < n:
+            next_fib = fib_iseq[-1] + fib_iseq[-2]
+            fib_iseq.append(next_fib)
+        return fib_iseq

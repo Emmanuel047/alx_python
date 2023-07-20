75291ef9b5b2c0732b2c214a855e102c3a5deb80
Emmanuel047 <emwendwa007@gmail.com>
2023-07-20 09:46:19 +0300

    4-new

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,8 +1,16 @@
-def is_prime(number):
-    if number <= 1:
-        return False
-    for i in range(2, int((number**0.5)) + 1):
-        if number % i == 0:
-            return False
-        return True
-    
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+
+    fibo_sequence = [0, 1]
+
+    while len(fibo_sequence) < n:
+        next_number = fibo_sequence[-1] + fibo_sequence[-2]
+        fibo_sequence.append(next_number)
+
+    return fibo_sequence
+

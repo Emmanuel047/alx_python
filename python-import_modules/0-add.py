a8af5e8dc1b258d79bee1bc48aeee59a55466602
Emmanuel047 <emwendwa007@gmail.com>
2023-07-25 14:26:21 +0300

    new4

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,10 +3,11 @@ from add_0 import add
 a = 1
 b = 2
 
+
 def main():
-  result = add(a, b)
-  print("{} + {} = {}".format(a, b, result))
-  
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+
+
 if __name__ == '__main__':
-  main()
-  
+    main()

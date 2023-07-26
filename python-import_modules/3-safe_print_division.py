59b1fb1188eb42d4667b6d47a0d7077dc9e4815c
Emmanuel047 <emwendwa007@gmail.com>
2023-07-26 15:20:33 +0300

    3-safe

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
-from divison import safe_print_division
-a = 12
-b = 2
-
-
-def main():
-    result = safe_print_division(a, b)
-    print("Inside result: {:d} / {:d} = {:d}".format(a, b, result))
-
-    if __name__ == '__main__':
-        main()
+def safe_print_division(a, b):
+    try:
+        answer = a/b
+    except ZeroDivisionError:
+        result = "None"
+    finally:
+        print("Inside result: {}", format(answer))
+        return answer

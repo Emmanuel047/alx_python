792205195aeb315c9cead29b80d8df3e99a0700d
Emmanuel047 <emwendwa007@gmail.com>
2023-07-26 15:40:19 +0300

    3-safe

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         answer = a/b
     except ZeroDivisionError:
-        result = "None"
+        answer = "None"
     finally:
         print("Inside result: {}".format(answer))
         return answer

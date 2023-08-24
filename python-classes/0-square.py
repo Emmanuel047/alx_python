92b6ad602e8379ee2695c46c92f70135fdf905c8
Emmamnuel047 <emwendwa007@gmail.com>
2023-08-24 10:42:41 +0300

    Squares

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,4 @@
 class Square:
+# Square class defining square attributes
     def __init__(self, size):
         self.__size = size
-    
-    def area(self):
-        return self.__size ** 2
-
-# Instantiate the Square class
-square1 = Square(5)
-print("Area of square1:", square1.area())  # Output: Area of square1: 25
-
-square2 = Square(7)
-print("Area of square2:", square2.area())  # Output: Area of square2: 49
